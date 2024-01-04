f00cde7640cb5dec2c8c4e6ff0cf23e396932e07
Miguel Varona <146460162+MigVarona@users.noreply.github.com>
2024-01-04 19:58:47 +0100

    Actualizar Hero.jsx

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
 			<img src="/10.jpg" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
 		</div>
 		<div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-			<a href="/Picture"><h1 className="text-5xl font-bold text-slate-200 leadi sm:text-6xl">Explore the wonders of space and Earth with NASA daily.
+			<a href="/Picture"><h1 className="text-5xl font-bold text-slate-200 leadi sm:text-6xl">Explore the wonders of space with NASA daily.
 				<span className="dark:text-violet-400"></span>
 			</h1>
 			<p className="mt-6 mb-8 text-lg sm:mb-12">
